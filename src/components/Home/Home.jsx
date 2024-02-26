05d4b4a6f44946496863addc09e3ca41a8f5acb0
Richard Andree Ramos Benites <andreerb_1997@outlook.com>
2024-02-25 23:02:26 -0500

    fix error home

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,13 +1,18 @@
+import React, { useEffect } from 'react';
 import Banner from '../Banner/Banner';
-import SummaryCard from '../SummaryCard/SummaryCard'
+import SummaryCard from '../SummaryCard/SummaryCard';
 
 const Home = () => {
+    useEffect(() => {
+        // Esta función se ejecutará cuando el componente se monte
+        window.scrollTo(0, 0); // Hace que la página se desplace hacia arriba al montar el componente
+    }, []); // El array vacío asegura que el efecto solo se ejecute una vez al montar el componente
+
     return (
         <div>
             <SummaryCard title={"Lugares Cercanos"} array={"nearvy"} />
             <Banner />
             <SummaryCard title={"Atracciones turísticas"} array={"places"} />
-           
         </div>
     );
 }
